c30d751cb2ad38f679048c394353c28e6a0fa878
ursSatish <satish4pandu@gmail.com>
2021-03-26 17:47:51 +0530

    added kendo packages

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,10 +5,50 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { IrisModule } from 'iris';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import 'hammerjs';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { EditorModule } from '@progress/kendo-angular-editor';
+import { ExcelExportModule } from '@progress/kendo-angular-excel-export';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { MenuModule } from '@progress/kendo-angular-menu';
+import { NotificationModule } from '@progress/kendo-angular-notification';
+import { ProgressBarModule } from '@progress/kendo-angular-progressbar';
+import { RippleModule } from '@progress/kendo-angular-ripple';
+import { SchedulerModule } from '@progress/kendo-angular-scheduler';
+import { SortableModule } from '@progress/kendo-angular-sortable';
+import { TooltipModule } from '@progress/kendo-angular-tooltip';
+import { UploadModule } from '@progress/kendo-angular-upload';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, IrisModule],
+  imports: [BrowserModule, AppRoutingModule, IrisModule, ButtonsModule, BrowserAnimationsModule, ChartsModule, DateInputsModule, DialogsModule, DropDownsModule, EditorModule, ExcelExportModule, GridModule, LabelModule, MenuModule, NotificationModule, ProgressBarModule, RippleModule, SchedulerModule, SortableModule, TooltipModule, UploadModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
